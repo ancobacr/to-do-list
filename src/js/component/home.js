6e2637f3a3a732560dbc3edde4d95a1dead42cc9
ancobacr <ancobacr@gmail.com>
2021-05-01 09:03:21 +0000

    updated

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function InputTask(/* { addTodo } */) {
+	const [value, setValue] = useState("");
 
-//create your first component
-export function Home() {
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (!value) return;
+		addTodo(value);
+		setValue("");
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				placeholder="what needs to be done?"
+				className="input"
+				value={value}
+				onChange={e => setValue(e.target.value)}
+			/>
+		</form>
+	);
+}
+
+function Todo(/* { todo, index, removeTodo } */) {
+	return (
+		<div
+			className="todo"
+			style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+			{todo.text}
+
+			<div>
+				<button onClick={() => removeTodo(index)}>x</button>
+			</div>
+		</div>
+	);
+}
+
+export function ToDoList() {
+	const [todos, setTodos] = useState([
+		{
+			text: "Learn about React",
+			isCompleted: false
+		}
+	]);
+
+	const addTodo = text => {
+		const newTodos = [...todos, { text }];
+		setTodos(newTodos);
+	};
+
+	const removeTodo = index => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="container">
+			<div className="">
+				<InputTask addTodo={addTodo} />
+				{todos.map((todo, index) => (
+					<Todo
+						key={index}
+						index={index}
+						todo={todo}
+						removeTodo={removeTodo}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
