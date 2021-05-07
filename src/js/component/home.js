7b8b23e4aa5210b1f0870cbf3193de358937b1b6
ancobacr <ancobacr@gmail.com>
2021-05-07 10:08:28 +0000

    todolist ready

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Button } from "react-bootstrap";
 
 export const TodoList = () => {
-	const [userInput, setUserInput] = useState("");
+	const [userInput, setUserInput] = useState(" ");
 	const [task, setTask] = useState([]);
 	const [count, setCount] = useState(0);
 
@@ -15,37 +15,49 @@ export const TodoList = () => {
 		}
 	};
 
-	const removeTask = id => {
+	const deleteTask = id => {
 		task.splice(id, 1);
 		setTask([...task]);
 		setCount(count - 1);
 	};
 
 	return (
-		<Container>
-			<Card>
-				<Card.Title>
-					<span className="h1">Todo List</span>
-				</Card.Title>
-				<input
-					className="mb-2 inputClass"
-					placeholder="Add Task"
-					type="text"
-					requiered
-					value={userInput}
-					onChange={e => setUserInput(e.target.value)}
-					onKeyPress={e => (e.key === "Enter" ? handleInfo() : "")}
-				/>
-				{task.map((final, id) => (
-					<span className="card-subtitle mb-4 claseTask" key={id}>
-						{final}
-						<button onClick={() => removeTask(id)}>X</button>
-					</span>
-				))}
-				<p id="counter" className="">
-					{count} task's left
-				</p>
-			</Card>
+		<Container className="justify-content-center">
+			<Container className="text-center mt-5">
+				<Card style={{ width: "25rem" }}>
+					<Card.Title>
+						<span className="h1">Todo List</span>
+					</Card.Title>
+
+					<input
+						className="mb-2 inputClass"
+						placeholder="what needs to be done?"
+						type="text"
+						requiered
+						value={userInput}
+						onChange={e => setUserInput(e.target.value)}
+						onKeyPress={e =>
+							e.key === "Enter" ? handleInfo() : ""
+						}
+					/>
+					{task.map((final, id) => (
+						<span className="card-subtitle mb-4 claseTask" key={id}>
+							{final}
+							<Button
+								variant="light"
+								onClick={() => deleteTask(id)}>
+								X
+							</Button>
+						</span>
+					))}
+
+					<Card.Footer>
+						<p id="counter" className="text-muted ">
+							{count} task's left
+						</p>
+					</Card.Footer>
+				</Card>
+			</Container>
 		</Container>
 	);
 };
